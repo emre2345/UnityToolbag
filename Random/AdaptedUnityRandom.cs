d789012435a0b3b5a7ce128d3e8809a4c6f8b8e2
Emre Solak <s.emresolak@dreamharvesters.com>
2018-03-11 16:37:24 +0300

    added random added list shuffle

---
+++ Random/AdaptedUnityRandom.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace UnityToolbag
+{
+    public class AdaptedUnityRandom : ICreateRandom
+    {
+        public int Seed
+        {
+            get { return Random.seed; }
+            set { Random.seed = value; }
+        }
+        
+        public float Next()
+        {
+            return Random.value;
+        }
+
+        public float Next(float max)
+        {
+            return Random.value * max;
+        }
+
+        public float Next(float min, float max)
+        {
+            return Random.Range(min, max);
+        }
+
+        public int Next(int min, int max)
+        {
+            return Random.Range(min, max);
+        }
+
+        public int Next(int max)
+        {
+            return Random.Range(0, max);
+        }
+    }
+}
---
+++ Random/ICreateRandom.cs
@@ -0,0 +1,12 @@
+﻿namespace UnityToolbag
+{
+    public interface ICreateRandom
+    {
+        int Seed { get; set; }
+        float Next();
+        float Next(float max);
+        float Next(float min, float max);
+        int Next(int min, int max);
+        int Next(int max);
+    }
+}
